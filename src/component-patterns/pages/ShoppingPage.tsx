d7a45a298b4d67e8747aae037816275d63f82e3a
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-07 18:22:08 -0300

    ProductTitle Component

--- src/component-patterns/pages/ShoppingPage.tsx
+++ src/component-patterns/pages/ShoppingPage.tsx
@@ -1,6 +1,7 @@
 import { ProductButtons } from "../components/ProductButtons";
 import ProductCard from "../components/ProductCard";
 import { ProductImg } from "../components/ProductImage";
+import { ProductTitle } from "../components/ProductTitle";
 
 const product = {
   id: "1",
@@ -15,6 +16,7 @@ const ShoppingPage = () => {
       <hr />
       <ProductCard product={product}>
         <ProductImg />
+        <ProductTitle title='Cafe'/>
         <ProductButtons/>
       </ProductCard>
     </div>
