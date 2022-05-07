f1a5652141817dd6d9d846a203f2092e99a62831
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-07 18:28:21 -0300

    Common index of Components

---
+++ src/component-patterns/components/index.ts
@@ -0,0 +1,17 @@
+import {ProductCard as ProductCardHOC} from './ProductCard';
+import { ProductImg } from './ProductImage';
+import { ProductTitle } from './ProductTitle';
+import { ProductButtons } from './ProductButtons';
+import { ProductCardHOCProps } from '../interfaces/interfaces';
+
+export { ProductButtons } from './ProductButtons';
+export { ProductImg } from './ProductImage';
+export { ProductTitle } from './ProductTitle';
+export const ProductCard : ProductCardHOCProps = Object.assign(ProductCardHOC, {
+
+    Title: ProductTitle,
+    Image: ProductImg,
+    Buttons: ProductButtons
+
+})
+
--- src/component-patterns/pages/ShoppingPage.tsx
+++ src/component-patterns/pages/ShoppingPage.tsx
@@ -1,11 +1,18 @@
-import { ProductButtons } from "../components/ProductButtons";
-import ProductCard from "../components/ProductCard";
-import { ProductImg } from "../components/ProductImage";
-import { ProductTitle } from "../components/ProductTitle";
+// import { ProductButtons } from "../components/ProductButtons";
+// import ProductCard from "../components/ProductCard";
+// import { ProductImg } from "../components/ProductImage";
+// import { ProductTitle } from "../components/ProductTitle";
+
+import {
+  ProductCard,
+  ProductButtons,
+  ProductImg,
+  ProductTitle,
+} from "../components";
 
 const product = {
   id: "1",
-  title: "Coffe",
+  title: "Coffe Hot",
   img: "./coffee-mug.png",
 };
 
@@ -14,11 +21,19 @@ const ShoppingPage = () => {
     <div>
       <h1> Shopping Store </h1>
       <hr />
-      <ProductCard product={product}>
-        <ProductImg />
-        <ProductTitle title='Cafe'/>
-        <ProductButtons/>
-      </ProductCard>
+      <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
+        <ProductCard product={product}>
+          <ProductCard.Image />
+          <ProductCard.Title />
+          <ProductCard.Buttons />
+        </ProductCard>
+
+        <ProductCard product={product}>
+          <ProductImg />
+          <ProductTitle title="Cafe" />
+          <ProductButtons />
+        </ProductCard>
+      </div>
     </div>
   );
 };
