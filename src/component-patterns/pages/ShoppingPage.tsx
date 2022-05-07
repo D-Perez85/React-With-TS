f7bf2f4ef454a6968475e78d050d83f0e90622ff
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-07 18:15:28 -0300

    Initial Components - Interfaces - Styles

--- src/component-patterns/pages/ShoppingPage.tsx
+++ src/component-patterns/pages/ShoppingPage.tsx
@@ -1,10 +1,21 @@
+import ProductCard from "../components/ProductCard";
+import { ProductImg } from "../components/ProductImage";
+
+const product = {
+  id: "1",
+  title: "Coffe",
+  img: "./coffee-mug.png",
+};
 
 const ShoppingPage = () => {
   return (
     <div>
-          <h1> Shopping Store  </h1>
-          <hr />
+      <h1> Shopping Store </h1>
+      <hr />
+      <ProductCard product={product}>
+        <ProductImg />
+      </ProductCard>
     </div>
-  )
-}
-export default ShoppingPage
+  );
+};
+export default ShoppingPage;
